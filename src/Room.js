4d8088ab3a11d49966278ac58e005a0c4e632515
kashifnawaz <kashi030@hotmail.com>
2021-04-14 13:45:25 +0500

    working

--- src/Room.js
+++ src/Room.js
@@ -1,13 +1,29 @@
-import React , {useState} from "react";
-
-
+import React, { useState } from 'react';
+import './Newd.css'
 
 function Room() {
+    let [isLit, setLit] = useState(false);
+    let [tem, setTem] = useState(72);
+
   return (
    <div>
 Please press the Button to turn On/Off lights.
+<br/>
+<button onClick>On</button>
+<button onClick>Off</button>
+<br/>
+The temprature of the room is : {tem} 
+<br/>
+please the button to Increase/Decrease Temp.
+<br/>
+<button onClick = { ()=> {
+        
+        setTem(++tem)}} >+</button>
+<button onClick = { ()=> {
+        
+        setTem(--tem)}} >-</button>
    </div>
   );
-}
 
+}
 export default Room;
